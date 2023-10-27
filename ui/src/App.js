641ef8f319d8874a2ad40ea8b248d6d30696edde
shivasaicharanruthala <ruthala.s@northeastern.edu>
2023-10-27 09:56:51 -0400

    Adds basic structure

---
+++ ui/src/App.js
@@ -0,0 +1,16 @@
+import * as React from "react";
+import {Routes, Route} from "react-router-dom";
+
+// imports App.js styles
+import './App.scss';
+
+// import all components to be rendered with routes.
+
+// App component is injected to index.html with all routes mapped to component to be rendered.
+function App() {
+  return (
+          <h1>Welcome</h1>
+  );
+}
+
+export default App;
